0cdd69543f8928b0debda3088ef498a08f1a5faa
shad <csadeebshad@gmail.com>
2020-06-14 13:01:26 +0600

    SideDrawer with Backdrop added

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,17 +2,29 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItem from '../NavigationItems/NavigationItems';
 import classes from './sideDrawer.css'
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux';
 
 const sideDrawer = (props) =>{
+
+    let attachedClass = [classes.SideDrawer, classes.Close]
+    if (props.open){
+        attachedClass = [classes.SideDrawer, classes.Open]
+    }
+
+
     return(
-        <div className = {classes.SideDrawer}>
-            <div className = {classes.Logo} >
-            <Logo />
-            </div>
-                <nav>
-                    <NavigationItem />
-                </nav>
-        </div>
+        <Aux>
+            <Backdrop show = {props.open} clicked = {props.closed}/>
+                <div className = {attachedClass.join(' ')}>
+                    <div className = {classes.Logo} >
+                    <Logo />
+                    </div>
+                        <nav>
+                            <NavigationItem />
+                        </nav>
+                </div>
+        </Aux>    
     );
 
 }
